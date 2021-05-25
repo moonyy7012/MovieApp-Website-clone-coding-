ac36cc9feb24d28428b2c77f7b78fae9ebf772e9
moonyy7012 <moonyy7012@naver.com>
2021-05-25 13:27:40 +0900

    Landing Page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
 import {API_URL, API_KEY, IMAGE_BASE_URL} from '../../Config';
 import MainImage from './Sections/MainImage';
+import GridCards from '../commons/GridCards';
+import {Row} from 'antd';
 
 // ''를 쓸때는
 // const userInfo = 'User info: ' + name + ' ' + surname + ' ' + telephone;
@@ -10,22 +12,27 @@ import MainImage from './Sections/MainImage';
 // 이렇게 ``를 쓰면 굳이 + + 를 사용하지 않고서도 구현이 가능해짐. 
 
 function LandingPage() {
-
-    // useState은 react에서 가져옴. 초기화후 response로 가져온 것들을
+    // const [<상태 값 저장 변수>, <상태 값 갱신 함수>] = useState(<상태 초기 값>);
+    //  useState은 react에서 가져옴. 초기화후 response로 가져온 것들을
     //  setMovies()를 통해 Movies state에 넣음
     const [Movies, setMovies] = useState([])
     const [MainMovieImage, setMainMovieImage] = useState(null)
 
+    // Movies state 변수를 선언한 뒤 리액트에게 effect를 사용함을 말함. useEffect Hook에 함수를 전달하고 있는데 이 함수가 바로 effect.
+    // 컴포넌트를 렌더링할 때 리액트는 우리가 이용한 effect를 기억하였다가 DOM을 업데이트한 이후에 실행함.
     useEffect(()=>{
-        // API_URL은 설정한 상수, 현재 인기있는 영화 불러옴
+        // API_URL은 설정한 상수. 현재 인기있는 영화 불러옴
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
         
         // fetch 는 가져오는 것, response는 그 결과값
         fetch(endpoint)
-        .then(response => response.json())
+        //fetch로는 데이터를 바로 사용할 수 없다. fetch를 사용할 땐 먼저 올바른 url로 요청을 보내야 하고, 바로 뒤에오는 응답에 대해 json()을 해줘야 한다.
+        //  json()은 Response 스트림을 가져와 스트림이 완료될때까지 읽는다. 그리고 다 읽은 body의 텍스트를 Promise형태로 반환한다.
+        .then(response => response.json()) //응답을 JSON 형태로 파싱합니다
         .then(response => {
-            console.log(response)
-            setMovies([response.results])
+            console.log(response.results)
+            //  Object 1Depth 이하의 요소들을 모두 가져오고 싶은 경우에 전개 연산자(...) 활용이 용이
+            setMovies(...[response.results])
             // 메인이미지는 인기있는 영화 가져온 response중 첫번째. 가장 인기 있는 영화의 이미지이므로
             setMainMovieImage(response.results[0])
         })
@@ -47,9 +54,26 @@ function LandingPage() {
             
             <div style={{width: '85%', margin: '1rem auto'}}>
                 <h2>Movies by latest</h2>
-                <hr />
+                {/* hr: 수평선 */}
+                <hr />  
                 {/*Movie Grid Cards */}
-            
+                {/* gutter : 사이 간격 */}
+                <Row gutter={[16,16]}>
+                    {/* movie는 Movies배열의 영화 데이터 하나. map은 그 movie하나하나에 대해 아래 함수를 실행하여 배열 변환  */}
+                    {Movies && Movies.map((movie, index)=>(
+                        // Fragments는 DOM에 별도의 노드를 추가하지 않고 여러 자식을 그룹화할 수 있습니다.key는 Fragment에 전달할 수 있는 유일한 어트리뷰트.
+                        <React.Fragment key={index}>
+                            {/* gridcards컴포넌트(GridCards.js) */}
+                            <GridCards
+                                image={movie.poster_path ? `${IMAGE_BASE_URL}w500${movie.poster_path}`:null}
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+                            />
+                        </React.Fragment>
+                    ))}
+
+                </Row>
+
             </div>
 
             <div style={{display: 'flex', justifyContent: 'center'}}>
@@ -61,3 +85,6 @@ function LandingPage() {
 }
 
 export default LandingPage
+
+
+
---
+++ client/src/components/views/commons/GridCards.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Col} from 'antd';
+
+// 반응형으로. 한 행의 size : 24. 창가장클때column4개->가장작을때1개 lg에서column크기6->xs에서column크기24
+function GridCards(props) {
+    return (
+        <Col lg={6} md={8} xs={24}>
+            <div style={{position: 'relative'}}>
+                <a href={`/movie/${props.movieId}`}>
+                    {/* <img>의 alt속성은 해당 이미지를 못 읽어올 때 대체하여 나타나는 것*/}
+                    <img style={{width: '100%', height: '320px'}} src={props.image} alt={props.movieName}/>
+                </a>
+            </div>
+        </Col>
+    )
+}
+
+export default GridCards
